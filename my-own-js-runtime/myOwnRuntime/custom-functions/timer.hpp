ba453c40a07946af0493e8bd94237f59294a3c7e
Michel Araujo <araujo_michel@yahoo.com.br>
2023-12-06 22:56:48 -0300

    MY_OWN_JS_RUNTIME: Config uv loop

--- my-own-js-runtime/myOwnRuntime/custom-functions/timer.hpp
+++ my-own-js-runtime/myOwnRuntime/custom-functions/timer.hpp
@@ -3,6 +3,8 @@ uv_loop_t *loop;
 
 struct timer {
     uv_timer_t uvTimer;
+    v8::Isolate *isolate;
+    v8::Global<v8::Function> callback;
 };
 
 class Timer {
@@ -24,13 +26,36 @@ class Timer {
             }
 
             timer *timerWrap = new timer();
+            timerWrap->callback.Reset(isolate, callback.As<v8::Function>());
             timerWrap->uvTimer.data = (void *)timerWrap;
-            
+            timerWrap->isolate = isolate;
+
             uv_timer_init(loop, &timerWrap->uvTimer);
             uv_timer_start(&timerWrap->uvTimer, onTimerCallback, sleep, interval);
         }
 
         static void onTimerCallback(uv_timer_t *handle) {
-            printf("## OnTimer ## \n");
+            timer *timerWrap = (timer *)handle->data;
+            v8::Isolate *isolate = timerWrap->isolate;
+            v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+            if (isolate->IsDead()) {
+                printf("Isolate is not alive");
+                return;
+            }
+
+            v8::Local<v8::Function> callback = v8::Local<v8::Function>::New(isolate, timerWrap->callback);
+            v8::Local<v8::Value> result;
+
+            auto callbackIsOK = callback->Call(
+                context,
+                v8::Undefined(isolate),
+                0,
+                NULL
+            ).ToLocal(&result);
+
+            if (!callbackIsOK) {
+                printf("Error to run callback function");
+            }
         }
 };
--- my-own-js-runtime/test.js
+++ my-own-js-runtime/test.js
@@ -6,5 +6,5 @@ mPrint('Sum 5 + 5 => ', result);
 mPrint('Wait for 5 seconds...');
 
 mTimeout(5000, 0, () => {
-    mprint('Run timeout: ', new Date().toISOString());
+    mPrint('Run timeout: ', new Date().toISOString());
 });
